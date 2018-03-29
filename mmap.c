95829fb1aca55457c8a367ae3eca653c56c52e96
Keshav Chand <keshavchand@protonmail.com>
2018-03-29 12:42:42 +0530

    Replaced brk with munmap

--- mmap.c
+++ mmap.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#define defaultSize 1024
 #define asm_code {0x48,0x89,0xf8,0x48,0x83,0xc0,0x00,0xc3}
 
 
@@ -9,14 +10,14 @@ typedef int (*rFunc)(int);
 rFunc func;
 
 void * get_virtual_memory(size_t size){
-    if (size < 1) size = 1024;
+    if (size < 1) size = defaultSize;
     int protection = PROT_READ | PROT_WRITE | PROT_EXEC; //We get a readable, writeable and executable memory
     int visibility = MAP_ANONYMOUS | MAP_SHARED; // Memory can be accessed by other process but mapping aren't backed by any file
     return mmap(NULL, size, protection , visibility, 0 , 0);
 }
 
-int inject_code(unsigned char code[], unsigned char * mem){
-    memcpy(mem , code, sizeof(code) );
+int inject_code(unsigned char code[], unsigned char * mem, int len){
+    memcpy(mem , code, len );
     return 0;
 }
 
@@ -32,7 +33,7 @@ int allocate_RWX(char * instruction, int j){
     else if (strncmp("shr", instruction, 3) == 0){ code[4] = 0xc1 ; code[5] = 0xe8 ; }
     code[6] = j % 255;
     void * mem = get_virtual_memory(16);
-    inject_code(code, mem);
+    inject_code(code, mem , sizeof(code));
     func = mem;
     return 1;
 }
@@ -49,6 +50,6 @@ int main(int argc, char * argv[]){
 
     printf("%d\n", func(i));
 
-    brk(func);
+    munmap(func, 16);
     return 0;
 }
